8fecd771f698dd4349676b0e183ad58edacdf813
Mustafa <mustafags435@gmail.com>
2024-03-02 18:28:04 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const fn = function name(params) {
-  console.log('Function working');
-};
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//middlewares
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'Temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
